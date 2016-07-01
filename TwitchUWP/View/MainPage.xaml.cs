2e9016193cdec0c0d04c67d984411243197ef798
tamkovip <tamkovip@RWAMW36281>
2016-07-01 17:09:58 +0200

    new style

--- TwitchUWP/View/MainPage.xaml.cs
+++ TwitchUWP/View/MainPage.xaml.cs
@@ -7,7 +7,7 @@ using TwitchUWP.ViewModel;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace TwitchUWP
+namespace TwitchUWP.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
